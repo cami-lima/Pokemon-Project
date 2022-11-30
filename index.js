74055817318cc5d76ab3758800e32c1db851eb59
Camila Maria de Lima <camilamarialima@gmail.com>
2022-11-30 11:03:33 -0300

    save pokemon info

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function buttonFunction() {
-    var b = document.createElement('BUTTON');
+    //  let b = document.createElement('BUTTON');
     window.location.assign('http://127.0.0.1:5500/searchpage.html');
 }
 
@@ -9,6 +9,7 @@ function getRandomNumber(min, max){
     return `https://pokeapi.co/api/v2/pokemon/${random}`
 }      
 
+let pokemonSearched = {};
 
 function loadpk() {
     let url = getRandomNumber(1, 807);
@@ -18,11 +19,13 @@ function loadpk() {
         return response.json();
     })
     .then((data)=> {
+        pokemonSearched = data;
     
-        document.querySelector('h3.nome').innerHTML = data['name'];
+        document.querySelector('h3.nome').innerHTML = data['name'].toUpperCase();
         for(let energia = 0; energia < data.stats.length; energia++ ){
             document.querySelector('p.HP').innerHTML = data.stats[energia].base_stat + ' HP';
         }
+        console.log(data)
 
         document.querySelector('p.weight').innerHTML = data['weight'] + ' WEIGHT';
         document.querySelector('p.height').innerHTML = data['height'] + ' HEIGHT';
@@ -34,15 +37,26 @@ function loadpk() {
             }
 
         let img = data['sprites']['front_default']
-        document.querySelector('img.pic').setAttribute('src', img);
+        document.querySelector('img.pic').setAttribute('src', img);    
     })
     .catch((erro) => {
         console.log('Erro' + erro);
     });
-   
+
 }
 
-document.createElement('btn1');
+function save(){
+    console.log('adicionou!')
+    console.log(pokemonSearched )
+    localStorage.setItem('infoPokemon', JSON.stringify(pokemonSearched));
+    let info = localStorage.getItem('infoPokemon');
+}
+
+
+
+
+
+// document.createElement('btn1');
 
 
 
